e79cf9b7ddeb08d4b1dbcbaea3400c4394172d41
Matthew Mason <jmmason@utexas.edu>
2023-03-03 11:04:46 -0600

    simple api for naive method

--- api/api.py
+++ api/api.py
@@ -1,12 +1,48 @@
 #!/bin/python3
 
-from flask import Flask
+import json
+from flask import Flask, request
+from flask_expects_json import expects_json
+
+from model.naive_model import Geometry, MicroChannelCooler
+from model.fluids import water
 
 app = Flask(__name__)
+
+schema = {
+    "type": "object",
+    "properties": {
+        "L": {"type":"number"},
+        "W": {"type":"number"},
+        "D": {"type":"number"},
+        "T_in": {"type":"number"},
+        "T_w": {"type":"number"},
+        "Q": {"type":"number"},
+        },
+    "required": ["L","W","D","T_in","T_w","Q"]
+    }
 
 @app.route("/")
 def hello_world():
     return "<center><h1>Hello, World!</h1></center>"
 
+@app.route("/naive", methods=['POST'])
+@expects_json(schema)
+def naive():
+
+    data = request.get_json()
+
+    geom = Geometry(data['L'], data['W'], data['D'])
+    cooler = MicroChannelCooler(geom, water, data['T_in'], data['T_w'], data['Q'])
+
+    q, dP, T_out = cooler.solve()
+    
+    return json.dumps({
+        'input': data,
+        'q' : q * 1e-4, # return q in W/cm^2
+        'dP' : dP * 0.000145038, # return dP in PSI
+        'T_out' : T_out - 273 # return T_out in C
+        }, indent=2)
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ api/example_post.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+curl -X POST http://127.0.0.1:5000/naive \
+     -H "Content-Type: application/json" \
+     -d @example_post.json
