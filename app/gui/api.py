62b496bdd9fa06ca6a2250dff99a141686b92194
Akhil Sadam <sadam.akhil@gmail.com>
2023-03-04 19:14:45 -0600

    full-page

--- app/gui/api.py
+++ app/gui/api.py
@@ -12,8 +12,9 @@ man = mp.Manager()
 data = None # global variables to store data (a bit hacky, but this way we should have a loading screen)
 state = [man.Value('i',0), man.Value('i',0), man.Value('i', 0), man.dict({'out':"Not Computed Yet"})] # iteration, total-iterations, status, result
 states = ['Idle', 'Running', 'Done']
-template_kwargs = {'template':"main", 'proxy':''}
-
+rf = 1.0
+inf = 1e15
+template_kwargs = {'template':"main", 'proxy':'', 'refresh_period': inf}
 # https://blog.miguelgrinberg.com/post/dynamically-update-your-flask-web-pages-using-turbo-flask
 @gui.route('/naive', methods=['GET','POST'])
 def naive() -> str:    
@@ -22,13 +23,18 @@ def naive() -> str:
     if rq.method == 'POST' and state[0].value == 0:
         data = js.loads(rq.form['inpt'])
         mp.Process(target=method, args=['/model/naive', data, *state]).start()
+        template_kwargs['refresh_period'] = rf
         return render_template("blank.jinja2", display="Loading ...", **template_kwargs)
 
     elif data is None:
         return render_template("form.jinja2", title="Naive Method Input", **template_kwargs)
     
     else: 
-        output = f"<b> Status: {states[state[2].value]} </b> <br> {'Iteration' if state[2].value != 2 else 'Result'} : {f'{state[0].value}/{state[1].value}' if state[2].value != 2 else state[3]['out']}"
+        if state[2].value != 2:
+            output = f"<b> Status: {states[state[2].value]} </b> <br> Iteration : {state[0].value}/{state[1].value}"
+        else:
+            template_kwargs['refresh_period'] = inf
+            output = f"<b> Status: {states[state[2].value]} </b> <br> Result : {state[3]['out']}"
         return render_template("blank.jinja2", display=output, **template_kwargs)
 
 
