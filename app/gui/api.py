91c242305bcf795d4ce14b565236e81b25f2204e
Akhil Sadam <sadam.akhil@gmail.com>
2023-03-04 19:07:48 -0600

    finally working

--- app/gui/api.py
+++ app/gui/api.py
@@ -1,78 +1,60 @@
 from time import sleep
+import multiprocessing as mp
+import ctypes
 from flask import Blueprint, request as rq, render_template, redirect
 import requests as rqs
 import json as js
 from config import base
 
 gui = Blueprint('gui', __name__)
+man = mp.Manager()
 
 data = None # global variables to store data (a bit hacky, but this way we should have a loading screen)
-job = None
-
-form = lambda x : """<div class="row center">
-  <div class="text leftAnimate">
-    <h1>
-      """+x+"""
-    </h1>
-    <div style="min-height: 5vh; min-width: 50vw;">
-    <!-- https://github.com/akhilsadam/compose-dev/blob/main/app/templates/io.jinja2 for more examples -->
-    <form action="" method="post">
-          <div style="display: flex; justify-content: space-evenly; width: 100%; margin-right:10px;">
-              <i class="fa-solid fa-plus">add</i>
-              <i class="fa-solid fa-arrows-spin">modify</i>
-              <i class="fa-solid fa-xmark">delete</i>
-          </div>
-          <br>
-          <div>   
-              <textarea type="text" id="inpt" name="inpt" title="JSON Input"
-              minlength="10" maxlength="999999" style="width:98.2%; height: 250px;  padding: 1px; resize: vertical;" 
-              placeholder="{"L":0.1, "W":100e-6, "D":10e-6, "T_in":293, "T_w":373, "Q":100}" style="width: 98%; ">
-              </textarea>
-	        </div>
-          <input type="submit" value="Submit">
-        </form>
-    </div>
-  </div>
-</div>"""
-loading = """<div class="row center">
-    <h1>
-      Loading...
-    </h1>
-    </div>
-"""
-
+state = [man.Value('i',0), man.Value('i',0), man.Value('i', 0), man.dict({'out':"Not Computed Yet"})] # iteration, total-iterations, status, result
+states = ['Idle', 'Running', 'Done']
+template_kwargs = {'template':"main", 'proxy':''}
 
 # https://blog.miguelgrinberg.com/post/dynamically-update-your-flask-web-pages-using-turbo-flask
 @gui.route('/naive', methods=['GET','POST'])
-def naive() -> str:
-    if rq.method == 'POST':
-        global data
+def naive() -> str:    
+    global data, state
+
+    if rq.method == 'POST' and state[0].value == 0:
         data = js.loads(rq.form['inpt'])
-    return render_template(
-        "blank.jinja2",
-        template="main",
-        proxy='',
-    )
-    # notice the following line is missing:
-    # blank = form("Naive Method Input"),
-    # this will come from the context_processor...
+        mp.Process(target=method, args=['/model/naive', data, *state]).start()
+        return render_template("blank.jinja2", display="Loading ...", **template_kwargs)
 
-@gui.context_processor
-def inject_naive():
-    global data, job
+    elif data is None:
+        return render_template("form.jinja2", title="Naive Method Input", **template_kwargs)
+    
+    else: 
+        output = f"<b> Status: {states[state[2].value]} </b> <br> {'Iteration' if state[2].value != 2 else 'Result'} : {f'{state[0].value}/{state[1].value}' if state[2].value != 2 else state[3]['out']}"
+        return render_template("blank.jinja2", display=output, **template_kwargs)
 
-    if data is None and job is None:
-        return dict(blank = form("Naive Method Input"))
-    
-    elif data != None and job is None:
-        job = '/model/naive', data
-        return dict(blank = loading)
-    
-    else: # have job
-        # start job
-        res = rqs.post(f'{base}{job[0]}', json=job[1])
-        sleep(5) # wait for 5 seconds
-        # upon end
-        job = None
-        data = None
-        return dict(blank = res.text)
+
+def method(*args):
+    cstate = args[2:]
+    nit = 10
+    cstate[1].value = nit
+    cstate[2].value = 1
+    # fake iteration to simulate a long running job
+    for iteration in range(nit):
+        cstate[0].value = iteration
+        sleep(0.5)
+
+    result = rqs.post(f'{base}{args[0]}', json=args[1])
+
+    cstate[2].value = 2
+    cstate[3]['out'] = result.text
+
+@gui.route('/<method>/clear', methods=['GET','POST'])
+def clear(method) -> str:
+    global data, state
+    data = None
+    state[0].value = 0
+    state[1].value = 0
+    state[2].value = 0
+    state[3]['out'] = "Not Computed Yet"
+    return redirect(f'/{method}')
+
+
