c5b28207ac52bfdd556ddcc45616c513fcb8c3f6
Matthew Mason <jmmason@utexas.edu>
2023-03-08 17:20:16 -0600

    add range functionality to api

--- app/model/api.py
+++ app/model/api.py
@@ -1,9 +1,11 @@
-import json
+import json, copy, io
+import numpy as np
 from flask import Blueprint, request
 from flask_expects_json import expects_json
 from model import schemes
 from model.naive_model import Geometry, MicroChannelCooler
 from model.fluids import water
+from model.tools import preprocess_input, convert_numpy_to_list
 
 model = Blueprint('model', __name__)
 
@@ -11,17 +13,23 @@ model = Blueprint('model', __name__)
 @expects_json(schemes.naive)
 def naive():
 
-    data = request.get_json()
-
-    geom = Geometry(data['L'], data['W'], data['D'])
+    global plot
+    
+    raw_data = request.get_json()
+    data = preprocess_input(raw_data)
+    
+    geom = Geometry( data['L'], data['W'], data['D'] )
     cooler = MicroChannelCooler(geom, water, data['T_in'], data['T_w'], data['Q'])
 
     q, dP, T_out = cooler.solve()
-    
-    return json.dumps({
-        'input': data,
+
+    out = {
+        'input': raw_data,
         'q' : q * 1e-4, # return q in W/cm^2
         'dP' : dP * 0.000145038, # return dP in PSI
         'T_out' : T_out - 273 # return T_out in C
-        }, indent=2)
+    }
+
+    convert_numpy_to_list(out)
     
+    return json.dumps(out, indent=2)
--- app/model/schemes.py
+++ app/model/schemes.py
@@ -1,12 +1,12 @@
 naive = {
     "type": "object",
     "properties": {
-        "L": {"type":"number"},
-        "W": {"type":"number"},
-        "D": {"type":"number"},
-        "T_in": {"type":"number"},
-        "T_w": {"type":"number"},
-        "Q": {"type":"number"},
+        "L": {"type":["number", "object"]},
+        "W": {"type":["number","object"]},
+        "D": {"type":["number","object"]},
+        "T_in": {"type":["number","object"]},
+        "T_w": {"type":["number","object"]},
+        "Q": {"type":["number","object"]},
         },
     "required": ["L","W","D","T_in","T_w","Q"]
     }
