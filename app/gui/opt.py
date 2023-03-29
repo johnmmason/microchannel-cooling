89d4b50bb5e2ccb9263bcd65787e26086c4f8c7c
Keri Christian <keri.emily.christian@gmail.com>
2023-03-29 13:11:32 -0500

    idk

--- app/gui/opt.py
+++ app/gui/opt.py
@@ -4,13 +4,18 @@ import plotly.graph_objects as go
 from dash import dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 from dash.exceptions import PreventUpdate
-from gui.dash_template import new_app
+from gui.dash_template import new_app, new_app_opt
 from model.naive_model import MicroChannelCooler, Geometry
 from model.fluids import water, ethylene_glycol, silicon_dioxide_nanofluid, mineral_oil
+from dash.long_callback import DiskcacheLongCallbackManager
 from config import update_style
 def make_naive_app_opt(server, prefix):
 
-    app = new_app(server, prefix, centered='center')
+    import diskcache
+    cache = diskcache.Cache("./cache")
+    long_callback_manager = DiskcacheLongCallbackManager(cache)
+
+    app = new_app_opt(server, prefix, long_callback_manager, centered='center')
     app.title = "Naive Model"
     app.version = 0.1
     # don't use H2 - that is reserved for dropdowns in Flask right now
@@ -33,77 +38,42 @@ def make_naive_app_opt(server, prefix):
                                   'Mineral oil'
                                   ], value = 'Water', id='fluid')],
                    ),
-                html.Div(["Length (m):", dcc.Input(id='length', value='0.1', type='text')]),
-                html.Div(["Width (um):", dcc.Input(id='width', value='100', type='text')]),
-                html.Div(["Depth (um):", dcc.Input(id='depth-from', value='10', type='text'),
-                        " to ", dcc.Input(id='depth-to', value='50', type='text')]),
-                html.Div(["Inlet Temperature (C):", dcc.Input(id='temp-inlet', value='20', type='text')]),
-                html.Div(["Wall Temperature (C):", dcc.Input(id='temp-wall', value='100', type='text')]),
-                html.Div(["Flow Rate (uL/min):", dcc.Input(id='flow-rate', value='100', type='text')]),
-                ], className='input'),
-                html.Div(className='hspace'),
-                html.Div([dcc.Graph(id='plot')], className='plot'),
+                html.Div([
+                html.P(id="paragraph_id", children=["Button not clicked"]),
+                html.Progress(id="progress_bar"),
+                ]),
+                html.Button(id="button_id", children="Run Job!"),
+                html.Button(id="cancel_button_id", children="Cancel Running Job!"),
+        ])
+
         ], className='row'),		
     ])
 
-    @app.callback(
-        Output(component_id='plot', component_property='figure'),
-        Input('fluid','value'),
-        Input('length','value'),
-        Input('width','value'),
-        Input('depth-from','value'),
-        Input('depth-to','value'),
-        Input('temp-inlet','value'),
-        Input('temp-wall','value'),
-        Input('flow-rate', 'value')
+    @app.long_callback(
+        output=Output("paragraph_id", "children"),
+        inputs=Input("button_id", "n_clicks"),
+        running=[
+            (Output("button_id", "disabled"), True, False),
+            (Output("cancel_button_id", "disabled"), False, True),
+            (
+                Output("paragraph_id", "style"),
+                {"visibility": "hidden"},
+                {"visibility": "visible"},
+            ),
+            (
+                Output("progress_bar", "style"),
+                {"visibility": "visible"},
+                {"visibility": "hidden"},
+            ),
+        ],
+        cancel=[Input("cancel_button_id", "n_clicks")],
+        progress=[Output("progress_bar", "value"), Output("progress_bar", "max")],
     )
-    def update_output_div(fluid, length, width, depth_from, depth_to, temp_inlet, temp_wall, flow_rate):
 
-        try:
-            L = float(length) # length of microchannel [m]
-            W = float(width) * 1e-6 # width of microchannel [m]
-
-            if fluid == 'Water':
-                F = water
-            elif fluid == 'Ethylene glycol':
-                F = ethylene_glycol
-            elif fluid == 'Silicon dioxide nanofluid':
-                F = silicon_dioxide_nanofluid
-            elif fluid == 'Mineral oil':
-                F = mineral_oil
-            else:
-                F = water
-            
-            T_in = float(temp_inlet)  + 273 # temperature of inlet [K]
-            T_w = float(temp_wall) + 273 # temperature of wall [K]
-            Q = float(flow_rate) # flow rate [uL/min]m]
-
-            D_low = float(depth_from)
-            D_high = float(depth_to)
-
-            assert D_low < D_high
-            D = np.arange( D_low, D_high, 1 ) * 1e-6 #depth of microchannel [m]
-        except:
-            raise PreventUpdate
-
-        geom = Geometry(L, W, D)
-        cooler = MicroChannelCooler(geom, F, T_in, T_w, Q)
-        q, dP, T_out = cooler.solve()
-
-        fig = make_subplots(rows=1, cols=2, column_widths=[.5, .5])
-
-        fig.add_trace(row=1, col=1, trace=go.Scatter(x=D, y=q*1e-4))
-        fig.add_trace(row=1, col=2, trace=go.Scatter(x=D, y=dP*0.000145038))
-
-        fig.update_xaxes(title_text="Channel Depth (m)", row=1, col=1)
-        fig.update_xaxes(title_text="Channel Depth (m)", row=1, col=2)
-        fig.update_yaxes(title_text="Heat Flux (W/cm2)", row=1, col=1)
-        fig.update_yaxes(title_text="Backpressure (psi)", row=1, col=2)
-
-        fig.update_layout(height=600, showlegend=False)
-        update_style(fig)
-            
-        return fig
-
-    return app.server
+    def callback(set_progress, n_clicks):
+        total = 10
+        for i in range(total):
+            time.sleep(0.5)
+            set_progress((str(i + 1), str(total)))
+        return [f"Clicked {n_clicks} times"]
 
