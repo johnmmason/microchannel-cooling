e478953951a61229c8a584f25f49a26a67f7cb6a
Keri Christian <keri.emily.christian@gmail.com>
2023-03-29 13:28:44 -0500

    got rid of the import * (STILL NOT WORKING)

--- app/api.py
+++ app/api.py
@@ -25,7 +25,7 @@ app.register_blueprint(gui, url_prefix='/')
 
 # dashapp example
 from gui.naive_app import make_naive_app
-from gui.opt import *
+from gui.opt import make_naive_app_opt
 with app.app_context():
     app = make_naive_app(app, '/naive/')
     app = make_naive_app_opt(app, '/opt/')
