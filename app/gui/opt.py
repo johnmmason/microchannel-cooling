7f3442622ab15b4c334b142985ae9838551634fd
Keri Christian <keri.emily.christian@gmail.com>
2023-03-29 18:00:59 -0500

    Uses Long's optimization and does progress bar for callback (Right now not connected to checkboxes of which to optimize)

--- app/gui/opt.py
+++ app/gui/opt.py
@@ -10,7 +10,10 @@ from model.naive_model import MicroChannelCooler, Geometry
 from model.fluids import water, ethylene_glycol, silicon_dioxide_nanofluid, mineral_oil
 from dash.long_callback import DiskcacheLongCallbackManager
 from config import update_style
+from model.sgd_model import sgd_model
+from model.sgd_model import SGD_MicroChannelCooler
 import time
+
 def make_naive_app_opt(server, prefix):
 
     cache = diskcache.Cache("./cache")
@@ -24,6 +27,8 @@ def make_naive_app_opt(server, prefix):
         html.H1("Microchannel Cooling, Naive Method Optimization", className='rh-align'),
         html.Div([
             html.Div([
+
+                # Shruthi make checkboxes instead of radio buttons
                 html.Div(["Choose a parameter to optimize:",
                     dcc.RadioItems([
                                  {'label': 'Length', 'value': 'length'},
@@ -32,6 +37,7 @@ def make_naive_app_opt(server, prefix):
                                  {'label': 'No Optimization', 'value': 'no'},
                                  ], value = 'no', id = 'opt')],
                    ),
+                # I think this is important for user to choose
 		        html.Div(["Select a Fluid",
                     dcc.Dropdown(['Water',
                                   'Ethylene glycol',
@@ -39,12 +45,13 @@ def make_naive_app_opt(server, prefix):
                                   'Mineral oil'
                                   ], value = 'Water', id='fluid')],
                    ),
+                # Cassandre make textboxes for initial input and then maximum iterations/ tolerance for 
                 html.Div([
-                html.P(id="paragraph_id", children=["Button not clicked"]),
+                html.P(id="paragraph_id", children=["Run Optimization"]),
                 html.Progress(id="progress_bar"),
                 ]),
-                html.Button(id="button_id", children="Run Job!"),
-                html.Button(id="cancel_button_id", children="Cancel Running Job!"),
+                html.Button(id="button_id", children="Run", n_clicks=0),
+                html.Button(id="cancel_button_id", children="Cancel Running Job"),
         ])
 
         ], className='row'),		
@@ -72,10 +79,34 @@ def make_naive_app_opt(server, prefix):
     )
 
     def callback(set_progress, n_clicks):
-        total = 10
-        for i in range(total):
-            time.sleep(20)
-            set_progress((str(i + 1), str(total)))
-        return [f"Clicked {n_clicks} times"]
+        if n_clicks > 0:
+            L = 0.1 # length of microchannel [m]
+            W = 100e-6 # width of microchannel [m]
+            D = np.arange(10, 50, 1) * 1e-6 # depth of microchannel [m]
+
+            T_in = 20 + 273 # inlet temperature [K]
+            T_w = 100 + 273 # inlet temperature [K]
+
+            Q = 100 # flow rate [uL/min]
+
+    
+            # q_list = []
+            # dP_list = []
+            # T_out_list = []
+            i = 1
+            total = len(D)
+            for D_scalar in D:
+                geom = Geometry(L, W, D_scalar)
+                i+1
+                cooler = SGD_MicroChannelCooler(geom, ethylene_glycol, T_in, T_w, 100)
+                L_optimized, W_optimized, D_optimized = cooler.solve_sgd(parameter_choice = [], optimize_type='default')
+                # q_list.append(q)
+                # dP_list.append(dP)
+                # T_out_list.append(T_out)    
+                set_progress((str(i + 1), str(total)))
+            return [f"L: {L_optimized}, W: {W_optimized}, D: {D_optimized}"]
+        else:
+            return["Optimize"]
+
     return app.server
 
