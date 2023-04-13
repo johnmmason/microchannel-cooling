2a43f517442767b74f4ee5a1d653a5ce822a5467
Akhil Sadam <sadam.akhil@gmail.com>
2023-04-12 23:03:01 -0500

    update function spec again

--- app/model/lmd_heat.py
+++ app/model/lmd_heat.py
@@ -1,7 +1,7 @@
 import taichi as ti
 from model.lmd_resistance_functions import solid_to_solid, solid_to_fluid, fluid_to_fluid
 # these functions can be used in a taichi kernel
-# args: (self, i1,j1,k1, i2,j2,k2) (the two nodes to calculate the resistance between, note order-independent)
+# args: (self, i1,j1,k1, i2,j2,k2, ie,je,ke) (the two nodes to calculate the resistance between, and then the element index, note order-independent)
 
 
 @ti.kernel
