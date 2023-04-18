eab2d4fa4ee3c96fdd474451a4aeae6a8091fae5
Akhil Sadam <sadam.akhil@gmail.com>
2023-04-17 19:31:51 -0500

    bugfixes - need to work on heat_flux and geometry still

--- app/model/lmd_heat_flux.py
+++ app/model/lmd_heat_flux.py
@@ -4,9 +4,9 @@ def setup_heat_flux(heat_flux_function, geometry):
     for i in range(geometry.nx):
         for j in range(geometry.ny):
             for k in range(geometry.nz):
-                if geometry.isfluid[i,j,k] != 1: # if not fluid
+                if geometry.isfluid[i,j,k] != 0: # if not fluid
                     x,y,z = geometry.ijk_to_xyz_host(i,j,k)
-                    geometry.heat_flux[i,j,k] = heat_flux_function(x,y,z) / geometry.volume[i,j,k]
+                    geometry.heat_flux[i,j,k] = heat_flux_function(x,y,z)
                     
 if __name__ == '__main__':
     from lmd_geometry import Geometry
