a4866c5028e237927256375983c6ec1bf7721436
Akhil Sadam <sadam.akhil@gmail.com>
2023-04-17 20:02:23 -0500

    added normalization factor for heat_flux

--- app/model/lmd_heat_flux.py
+++ app/model/lmd_heat_flux.py
@@ -3,10 +3,15 @@ import taichi as ti
 def setup_heat_flux(heat_flux_function, geometry): 
     for i in range(geometry.nx):
         for j in range(geometry.ny):
+            vz = 0.0
+            for k in range(geometry.nz):
+                if geometry.isfluid[i,j,k] != 0: # if not fluid
+                    vz += geometry.volume[i,j,k]
+                    
             for k in range(geometry.nz):
                 if geometry.isfluid[i,j,k] != 0: # if not fluid
                     x,y,z = geometry.ijk_to_xyz_host(i,j,k)
-                    geometry.heat_flux[i,j,k] = heat_flux_function(x,y,z)
+                    geometry.heat_flux[i,j,k] = heat_flux_function(x,y,z) * geometry.volume[i,j,k] / vz
                     
 if __name__ == '__main__':
     from lmd_geometry import Geometry
